a33e993d2f4c41ce0d88ab2b2934480f04db5987
Lawrence <Lawrence4code@gmail.com>
2018-08-27 22:21:10 +0530

    Converted CRA to basic react app v1

--- crypolist/src/App.js
+++ crypolist/src/App.js
@@ -1,15 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
 
-import './styles/styles.css';
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1 className="App-title"> React App </h1>
-      </div>
-    );
-  }
-}
-
-export default App;
+ReactDOM.render(<p> React App Basic Setup </p>, document.getElementById('app'));
