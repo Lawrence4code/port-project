7fbcad439279f7bb4d5a1e4c2fd5767591c3647c
Lawrence <Lawrence4code@gmail.com>
2018-08-27 14:00:51 +0530

    CRYPOLIST - New project added with CRA, customized as per project

---
+++ crypolist/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import './styles/styles.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1 className="App-title"> React App </h1>
+      </div>
+    );
+  }
+}
+
+export default App;
